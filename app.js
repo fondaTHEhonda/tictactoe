e1b098eecbfbc3dfea7970c4c562e0bde9ef5573
fondaTHEhonda <casatchell15@gmail.com>
2021-02-21 14:25:34 -0500

    Added basic function for X or O to be added to board

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-//Gameboard module
-const gameBoard = (() => {
-    const board = [];
-});
-
-//Player Factory
-const Player = (name) => {
-    const getName = () => name;
-    return {name}
-};
-
-const player1 = Player("Conor");
-
 //Displaying HTML
 const displayController = (() => {
     const gameContainer = document.getElementById("game-container");
@@ -22,6 +9,38 @@ const displayController = (() => {
     }
 })();
 
-const controlGame = (() => {
+//Gameboard module
+const gameBoard = (() => {
+    const board = [];
 
-});
+    const controlGame = function(e) {
+        if(e.target.classList.contains("game-cell")) {
+            if(e.target.textContent === "")
+            e.target.textContent = prompt("X or O?");
+            board.push(e.target.textContent);
+            console.log(board);
+        }
+    };
+    return {controlGame};
+})();
+
+window.addEventListener('click', gameBoard.controlGame)
+
+
+//Player Factory
+const Player = (name, mark, turn) => {
+    const getName = () => name;
+    const getMark = () => mark
+    let isTurn = () => turn;
+
+    const changeTurn = () => {
+        
+    }
+
+    return {name, mark, turn}
+};
+
+const player1 = Player("Conor", "X", true);
+const player2 = Player("Leah", "O", false);
+
+
