d77f86f57548a589e21ccd37d710f6ab11983238
fondaTHEhonda <casatchell15@gmail.com>
2021-02-21 12:23:26 -0500

    Function to display gameboard added. Added flex box and game board style to CSS

---
+++ app.js
@@ -0,0 +1,23 @@
+//Gameboard module
+const gameBoard = (() => {
+    const board = [];
+});
+
+//Player Factory
+const Player = (name) => {
+    const getName = () => name;
+    return {name}
+};
+
+const player1 = Player("Conor");
+
+//Displaying HTML
+const displayController = (() => {
+    const gameContainer = document.getElementById("game-container");
+
+    for(let i = 0; i < 9; i++) {
+        let newDiv = document.createElement('div');
+        gameContainer.appendChild(newDiv);
+        newDiv.classList.add('game-cell');
+    }
+})();
