6ceb5ed438000914b9e93ccae839e6229db68a24
fondaTHEhonda <casatchell15@gmail.com>
2021-02-21 21:03:33 -0500

    Small changes

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ const displayController = (() => {
 //Gameboard module
 const gameBoard = (() => {
     const board = [];
+    const player1Moves = [];
+    const player2Moves = [];
+    const winningBoards = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+    ]; 
+
 
     const controlGame = function(e) {
         if(e.target.classList.contains("game-cell")) {
@@ -23,31 +34,22 @@ const gameBoard = (() => {
                 if(player1.turn === true) {
                     e.target.textContent = "X";
                     board.push(e.target.textContent);
+                    player1Moves.push(parseInt(e.target.id));
+                    console.log(player1Moves)
                     player1.turn = false;
                     player2.turn = true;
                 } else if(player2.turn === true) {
                     e.target.textContent = "O";
                     board.push(e.target.textContent);
+                    player2Moves.push(parseInt(e.target.id));
+                    console.log(player2Moves)
                     player2.turn = false;
                     player1.turn = true;
                 }
         }
     };
 
-    const checkWinner = function(boardId) {
-        const winningBoards = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-        ];
-
-        
-    }
-
-    return {controlGame};
+    return { controlGame, checkWinner}
 })();
 
 window.addEventListener('click', gameBoard.controlGame)
@@ -59,7 +61,7 @@ const Player = (name, mark, turn) => {
     const getMark = () => mark
     let isTurn = () => turn;
 
-    return {name, mark, turn}
+    return {name, mark, turn, getMark}
 };
 
 const player1 = Player("Conor", "X", true);
