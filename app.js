41af7995be3676741689ff539d481c50d0bba566
fondaTHEhonda <casatchell15@gmail.com>
2021-02-27 12:48:28 -0500

    Function to determine winner added

--- app.js
+++ app.js
@@ -27,44 +27,52 @@ const gameBoard = (() => {
         [2, 5, 8],
     ]; 
 
-    const checkWinner = function(playerMark) {
-        return winningBoards.some(function(threeInARow) {
-            return threeInARow.InARow.every(function(square) {
-                return board[square] === playerMark;
-            })
-        })
+    const checkWinner = function (e) {
+        for(let i = 0; i < winningBoards.length; i++) {
+            for(let j = 0; j < winningBoards[i].length; j++) {
+                if(winningBoards[i][j] === parseInt(e.target.id)) {
+                    winningBoards[i][j] = e.target.textContent
+                    console.log(winningBoards);
+                        if(winningBoards[i][0] === "X" && winningBoards[i][1] === "X" && winningBoards[i][2] === "X") {
+                        alert(`${player1.name} wins!`);
+                    } else if(winningBoards[i][0] === "O" && winningBoards[i][1] === "O" && winningBoards[i][2] === "O") {
+                        alert(`${player2.name} wins!`);
+                    }
+                }
+            }
+        }
     }
 
-    let xWin = checkWinner("X");
-    ley oWin = checkWinner("O")
 
     const controlGame = function(e) {
         if(e.target.classList.contains("game-cell")) {
             if(e.target.textContent === "")
-                if(player1.turn === true) {
-                    e.target.textContent = "X";
-                    board.push(e.target.textContent);
-                    player1Moves.push(parseInt(e.target.id));
-                    console.log(player1Moves)
-                    console.log(board)
-                    player1.turn = false;
-                    player2.turn = true;
-                } else if(player2.turn === true) {
-                    e.target.textContent = "O";
-                    board.push(e.target.textContent);
-                    player2Moves.push(parseInt(e.target.id));
-                    console.log(player2Moves)
-                    console.log(board)
-                    player2.turn = false;
-                    player1.turn = true;
+                        if(player1.turn === true) {
+                            e.target.textContent = "X";
+                            board.push(e.target.textContent);
+                            player1Moves.push(parseInt(e.target.id));
+                            console.log(player1Moves)
+                            console.log(board)
+                            player1.turn = false;
+                            player2.turn = true;
+                            
+                        } else if(player2.turn === true) {
+                            e.target.textContent = "O";
+                            board.push(e.target.textContent);
+                            player2Moves.push(parseInt(e.target.id));
+                            console.log(player2Moves)
+                            console.log(board)
+                            player2.turn = false;
+                            player1.turn = true;
                 }
-        }
+            }
     };
 
-    return {controlGame}
+    return {controlGame, checkWinner}
 })();
 
 window.addEventListener('click', gameBoard.controlGame)
+window.addEventListener('click', gameBoard.checkWinner)
 
 
 //Player Factory
